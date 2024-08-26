9836f389c827d5933826b139120ce53f20ca9036
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-26 15:26:38 +0300

    Added some markup items to the user button

--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -7,14 +7,30 @@ import {
 	DropdownMenuItem,
 	DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
+import { useCurrentUser } from '@/features/auth/hooks/use-current-user'
+import { Loader2Icon } from 'lucide-react'
 
 const UserButton = () => {
+	const { data, isLoading } = useCurrentUser()
+
+	if (isLoading) {
+		return (
+			<Loader2Icon className='size-10 animate-spin text-muted-foreground' />
+		)
+	}
+
+	if (!data) return null
+
+	const { image, name, email } = data
+
 	return (
 		<DropdownMenu modal={false}>
 			<DropdownMenuTrigger className='outline-none relative '>
 				<Avatar className='size-10 hover:opacity-75 transition'>
-					<AvatarImage />
-					<AvatarFallback>CV</AvatarFallback>
+					<AvatarImage src={image} alt={name} />
+					<AvatarFallback>
+						{name!.charAt(0).toUpperCase()}
+					</AvatarFallback>
 				</Avatar>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent align='center' side='right' className='w-60'>
