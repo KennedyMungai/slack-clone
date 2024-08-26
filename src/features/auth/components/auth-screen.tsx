359a9df67b0b6b8977b0f0b48e54084cd97a0736
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-26 09:55:58 +0300

    Implemented basic auth flow for a user using the application

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -1,9 +1,20 @@
 'use client'
 
-type Props = {}
+import { SignInFlow } from '@/features/auth/types'
+import { useState } from 'react'
+import SignInCard from './sign-in-card'
+import SignUpCard from './sign-up-card'
 
 const AuthScreen = () => {
-	return <div>AuthScreen</div>
+	const [state, setState] = useState<SignInFlow>('signIn')
+
+	return (
+		<div className='h-full flex items-center justify-center bg-[#5c3d58]'>
+			<div className='md:h-auto md:w-[420px]'>
+				{state === 'signIn' ? <SignInCard /> : <SignUpCard />}
+			</div>
+		</div>
+	)
 }
 
 export default AuthScreen
