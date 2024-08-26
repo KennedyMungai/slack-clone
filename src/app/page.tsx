f2443fa43882324bd5949a5e0c380c5abb20be8d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-26 16:00:30 +0300

    Workspace id shenanigans

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
+'use client'
+
 import UserButton from '@/features/auth/components/user-button'
+import { useGetWorkspaces } from '@/features/workspaces/api/use-get-workspaces'
+import { useEffect, useMemo } from 'react'
 
 const HomePage = () => {
+	const { data, isLoading } = useGetWorkspaces()
+
+	const workspaceId = useMemo(() => data?.[0]?._id, [data])
+
+	useEffect(() => {
+		if (isLoading) return
+
+		if (workspaceId) console.log('Redirect to workspace')
+		else console.log('Open Creation modal')
+	}, [workspaceId, isLoading])
+
 	return (
 		<div>
 			<UserButton />
