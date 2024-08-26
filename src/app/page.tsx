50d4abeef629cea0be7cb529a0b575a405f4f53f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-26 15:33:52 +0300

    Simplified the index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import UserButton from '@/features/auth/components/user-button'
+
 const HomePage = () => {
-	return <div>Logged In</div>
+	return (
+		<div>
+			<UserButton />
+		</div>
+	)
 }
 
 export default HomePage
