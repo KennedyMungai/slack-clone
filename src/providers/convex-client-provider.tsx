a76ee35f9bae6492a7d84d7557784ead61b957f8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-26 11:50:21 +0300

    Implemented convex auth for the application

--- src/providers/convex-client-provider.tsx
+++ src/providers/convex-client-provider.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { ConvexProvider, ConvexReactClient } from 'convex/react'
+import { ConvexAuthNextjsProvider } from '@convex-dev/auth/nextjs'
+import { ConvexReactClient } from 'convex/react'
 import { ReactNode } from 'react'
 
 type Props = {
@@ -10,7 +11,11 @@ type Props = {
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
 const ConvexClientProvider = ({ children }: Props) => {
-	return <ConvexProvider client={convex}>{children}</ConvexProvider>
+	return (
+		<ConvexAuthNextjsProvider client={convex}>
+			{children}
+		</ConvexAuthNextjsProvider>
+	)
 }
 
 export default ConvexClientProvider
