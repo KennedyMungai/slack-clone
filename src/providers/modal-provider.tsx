0bb8a4daffe9962f8d71f4decc2c3e35bd4bbd40
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-27 15:29:28 +0300

    Added a useState to the component to make sure it renders in the client to fix potential hydration errors

--- src/providers/modal-provider.tsx
+++ src/providers/modal-provider.tsx
@@ -1,8 +1,11 @@
 'use client'
 
 import CreateWorkspaceModal from '@/features/workspaces/components/create-workspace-modal'
+import { useState } from 'react'
 
 const ModalProvider = () => {
+	const [] = useState()
+
 	return (
 		<>
 			<CreateWorkspaceModal />
