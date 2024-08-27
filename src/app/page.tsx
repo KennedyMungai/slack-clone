7e756d677f5ce4072aecab0ee70250ddf50ff555
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-27 15:14:32 +0300

    Added the useCreateWorkspaceModal to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 
 import UserButton from '@/features/auth/components/user-button'
+import { useCreateWorkspaceModal } from '@/features/store/use-create-workspace-modal'
 import { useGetWorkspaces } from '@/features/workspaces/api/use-get-workspaces'
 import { useEffect, useMemo } from 'react'
 
 const HomePage = () => {
+	const [open, setOpen] = useCreateWorkspaceModal()
+
 	const { data, isLoading } = useGetWorkspaces()
 
 	const workspaceId = useMemo(() => data?.[0]?._id, [data])
@@ -13,8 +16,10 @@ const HomePage = () => {
 		if (isLoading) return
 
 		if (workspaceId) console.log('Redirect to workspace')
-		else console.log('Open Creation modal')
-	}, [workspaceId, isLoading])
+		else if (!open) {
+			setOpen(true)
+		}
+	}, [workspaceId, isLoading, open, setOpen])
 
 	return (
 		<div>
