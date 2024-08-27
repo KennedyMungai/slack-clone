6dae5b0f3da0b2a964befe3cbda2f9d86000ab10
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-27 15:26:56 +0300

    Created a modal provider and added it to the application

---
+++ src/providers/modal-provider.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import CreateWorkspaceModal from '@/features/workspaces/components/create-workspace-modal'
+
+const ModalProvider = () => {
+	return (
+		<>
+			<CreateWorkspaceModal />
+		</>
+	)
+}
+
+export default ModalProvider
